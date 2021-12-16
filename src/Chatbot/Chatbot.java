cf99c4b52f9a853e9d1ba1a6dc62a50a9a866dfe
Максименко Надя <n.a.maksymenko@student.khai.edu>
2021-12-17 00:06:25 +0200

    Chatbot 2 stage

--- src/Chatbot/Chatbot.java
+++ src/Chatbot/Chatbot.java
@@ -1,4 +1,5 @@
 package Chatbot;
+import java.util.Scanner;
 
 public class Chatbot {
     public String name;
@@ -12,12 +13,17 @@ public class Chatbot {
     public void printbot(){
         System.out.println("Hello! My name is " + name );
         System.out.println("I was created in " + year );
+        System.out.println("Please, remind me your name");
 
     }
 
     public static void main(String args[]){
         Chatbot empOne = new Chatbot("Aboba", 2021);
         empOne.printbot();
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        System.out.printf("What a great name you have, %s ", num + "!");
+        in.close();
     }
 }
 
