767db28e829906f6b2ddbac2d6f8bc37e6d8881f
Максименко Надя <n.a.maksymenko@student.khai.edu>
2021-12-22 22:03:22 +0200

    CoffeeMachine 3 stage

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = input.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = input.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = input.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
         int cups = input.nextInt();
-        System.out.println("For "+ cups + " cups of coffee you will need:\n");
-        int numWater = cups*200;
-        System.out.println(numWater + " ml of water\n");
-        int numMilk = cups*50;
-        System.out.println(numMilk + " ml of milk\n");
-        int numBeans = cups*15;
-        System.out.println(numBeans + " g of coffee beans\n");
+        int wa = water / 200;
+        int mi = milk / 50;
+        int be = beans / 15;
+        int x = Math.min(wa, mi);
+        int min = Math.min(x, be);
+
+        if (min < cups){
+            System.out.println("No, I can make only "+min+" cups of coffee");
+        } else if( min == cups){
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            int minimum = min - cups;
+            System.out.println("Yes, I can make that amount of coffee (and even " + minimum + " more than that)");
+        }
     }
 }
