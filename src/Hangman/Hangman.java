6abbc4564fe67ae3fcac79d2ceafa381a4478d54
Максименко Надя <n.a.maksymenko@student.khai.edu>
2021-12-25 15:04:20 +0200

    Hangman 6 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,4 +1,5 @@
 package Hangman;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -6,33 +7,38 @@ public class Hangman {
 
     public static void main(String[] args) {
 
-        String[] wordsList = {"java", "kotlin", "python", "javascript"};
-        String word = generateWord(wordsList);
-        StringBuilder secretWord = secretWordGen(word);
+        String[] wordsArr = {"java", "kotlin", "python", "javascript"};
+        String word = generateWord(wordsArr);
+        StringBuilder hiddenWord = hiddenWordGen(word);
 
 
         System.out.println("HANGMAN");
         int lives = 8;
         while (true) {
             if (lives == 0){
+                System.out.println("No improvements");
                 System.out.println("You lost!");
                 break;
             } else {
-                if (secretWord.indexOf("-") != -1) {
-                    System.out.printf("Input a letter %s:", secretWord);
+                if (hiddenWord.indexOf("-") != -1) {
+                    System.out.printf("Input a letter %s:", hiddenWord);
                     Scanner userInput = new Scanner(System.in);
                     String answer = userInput.nextLine();
                     if (!word.contains(answer)) {
                         System.out.println("The letter doesn't appear in word");
                         --lives;
                     }
+                    else if (hiddenWord.indexOf(answer) != -1){
+                        System.out.println("No improvements");
+                        --lives;
+                    }
                     else {
-                        secretWord = updateSecretWord(secretWord, answer, word);
+                        hiddenWord = updateHiddenWord(hiddenWord, answer, word);
                     }
                 } else {
-                    System.out.println(secretWord);
-                    System.out.println("Thanks for playing!");
-                    System.out.println("We'll see how well you did in the next stage");
+                    System.out.println(hiddenWord);
+                    System.out.println("You guessed the word!");
+                    System.out.println("You survived");
                     break;
                 }
             }
@@ -40,35 +46,28 @@ public class Hangman {
         }
     }
 
-    public static String generateWord(String[] wordsList) {
+    public static String generateWord(String[] wordsArr) {
         Random randomIntGen = new Random();
-        int randomInt = randomIntGen.nextInt(wordsList.length);
-        String word = wordsList[randomInt];
-        return wordsList[randomInt];
+        int randomInt = randomIntGen.nextInt(wordsArr.length);
+        return wordsArr[randomInt];
     }
 
-    public static StringBuilder secretWordGen(String word) {
-        StringBuilder secretWord = new StringBuilder("");
-        secretWord.append("-".repeat(word.length()));
-        return secretWord;
+    public static StringBuilder hiddenWordGen(String word) {
+        StringBuilder hiddenWord = new StringBuilder("");
+        hiddenWord.append("-".repeat(word.length()));
+        return hiddenWord;
     }
 
-
-    public static StringBuilder updateSecretWord(StringBuilder secretWord, String answer, String word) {
+    public static StringBuilder updateHiddenWord(StringBuilder hiddenWord, String answer, String  word) {
         int index = 0;
         while (index >= 0) {
             index = word.indexOf(answer, index);
             if (index >= 0) {
-                secretWord.setCharAt(index, answer.charAt(0));
+                hiddenWord.setCharAt(index, answer.charAt(0));
                 ++index;
             }
         }
 
-        return secretWord;
+        return hiddenWord;
     }
 }
-
-
-
-
-
